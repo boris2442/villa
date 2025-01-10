ebacaf58f0c065a48825ee861edee79b6ca3da0c
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-10 17:28:09 +0100

    chatgement javascript

--- javascript/script.js
+++ javascript/script.js
@@ -65,4 +65,11 @@ const sr=ScrollReveal({
   .start();
   
 
-  console.log("hjsjd");
+// chargement de la page
+
+window.onload = function() {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.loader').style.transition='5s ease-out'
+  };
+  
+  
